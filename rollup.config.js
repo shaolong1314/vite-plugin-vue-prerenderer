5cdd859b25d80ac124ea7b530b5b08774869dc35
shaolong <2715180590@qq.com>
2022-10-21 15:05:50 +0800

    优化压缩打包

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: shaolong
+ * @Date: 2022-10-21 14:52:48
+ * @LastEditors: shaolong
+ * @LastEditTime: 2022-10-21 15:03:36
+ * @Description:
+ */
+export default {
+  input: "index.js",
+  output: {
+    file: "dist/index.js",
+    format: "iife",
+    name: "bundleName",
+  },
+};
